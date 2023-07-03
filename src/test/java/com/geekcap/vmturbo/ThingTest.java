1b72e45954917dc6388840a8d239e7f6d36041ce
michael3823igberaese <133489978+michael3823igberaese@users.noreply.github.com>
2023-07-03 09:42:52 +0200

    Update ThingTest.java

--- src/test/java/com/geekcap/vmturbo/ThingTest.java
+++ src/test/java/com/geekcap/vmturbo/ThingTest.java
@@ -11,7 +11,7 @@ public class ThingTest {
     @Test
     public void testN() {
         Thing t = new Thing();
-        t.setN(25);
-        Assert.assertEquals("N should be 25", 25, t.getN());
+        t.setN(5);
+        Assert.assertEquals("N should be 5", 5, t.getN());
     }
 }
